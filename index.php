ed745a2a7a478694af099f9d54828688edbe5747
vince <vincent.harpin@orange.fr>
2023-01-10 15:13:44 +0100

    exercice 135

--- index.php
+++ index.php
@@ -6,14 +6,15 @@ echo "Un fichier php va être inclu à la suite de ce texte ...<br><br>";
 
 // inclure fichier texte.php ci dessous
 
+include "texte.php";
 
 // Deuxieme Ligne ( inclure fichiers vars.php ci dessous )
 
-
+require "vars.php";
 
 echo "J'aime également les " . $maVar;
 
-
+?>
 
 
 
